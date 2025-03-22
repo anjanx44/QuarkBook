5a6e8402024c028ebdcb0723b47ad1c843682488
anjanx44 <anjanx44@gmail.com>
2025-03-22 16:23:46 +0600

    added jwt in user-service

---
+++ user-service/src/main/java/org/anjanx44/userservice/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package org.anjanx44.userservice.dto;
+
+public class TokenResponse {
+    private String token;
+
+    public TokenResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
